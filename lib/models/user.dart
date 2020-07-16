9f0ff3c3b1bcfe8f76ca627c39875a602db0a1da
szatjp <szxatjp@163.com>
2020-07-16 16:29:55 +0800

    change

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -9,6 +9,11 @@ class User {
 
   String username;
   String email;
+  @JsonKey(name: 'first_name') String firstName;
+  @JsonKey(name: 'last_name') String lastName;
+  @JsonKey(name: 'last_login') String lastLogin;
+  @JsonKey(name: 'is_active') bool isActive;
+  dynamic nickname;
 
   factory User.fromJson(Map<String,dynamic> json) => _$UserFromJson(json);
   Map<String, dynamic> toJson() => _$UserToJson(this);
--- lib/models/user.g.dart
+++ lib/models/user.g.dart
@@ -9,10 +9,20 @@ part of 'user.dart';
 User _$UserFromJson(Map<String, dynamic> json) {
   return User()
     ..username = json['username'] as String
-    ..email = json['email'] as String;
+    ..email = json['email'] as String
+    ..firstName = json['first_name'] as String
+    ..lastName = json['last_name'] as String
+    ..lastLogin = json['last_login'] as String
+    ..isActive = json['is_active'] as bool
+    ..nickname = json['nickname'];
 }
 
 Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
       'username': instance.username,
       'email': instance.email,
+      'first_name': instance.firstName,
+      'last_name': instance.lastName,
+      'last_login': instance.lastLogin,
+      'is_active': instance.isActive,
+      'nickname': instance.nickname,
     };
